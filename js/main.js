e4036e300730ddc1dc48650ee53ee243e012f1c3
Aditya Chauhan <officialemail032@gmail.com>
2026-02-02 00:20:20 +0530

    created create.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 function goToCreate() {
   window.location.href = "create.html";
 }
+
+const form = document.getElementById("loveForm");
+
+if (form) {
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const yourName = document.getElementById("yourName").value;
+    const partnerName = document.getElementById("partnerName").value;
+    const message = document.getElementById("message").value;
+
+    const loveData = {
+      yourName,
+      partnerName,
+      message
+    };
+
+    // Save to localStorage
+    localStorage.setItem("loveData", JSON.stringify(loveData));
+
+    // Go to love page
+    window.location.href = "love.html";
+  });
+
+}
